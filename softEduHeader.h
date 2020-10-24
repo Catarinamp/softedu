c7e1cf93770da69e6442355d6d2c9cd1364b769b
abelaraujo <abel2901@gmail.com>
2020-10-24 12:15:38 -0300

    Cria cabeçalho

--- softEduHeader.h
+++ softEduHeader.h
@@ -1,1 +1,20 @@
+struct aluno
+{
+    char nome[100];
+    int matricula;
+    char disciplina[50];
+};
+typedef struct aluno Aluno;
+
+struct lista
+{
+    Aluno info;
+    struct lista *prox;
+};
+typedef struct lista Lista;
+
+Lista *inserir(Lista *l, Aluno a);
+Aluno cadastrar();
+void *exibir(Lista *l);
+void *pesquisar(Lista *l);
 
